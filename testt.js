62fdea0d3fa2d4842d227cdc26596c233f23390d
codeasch <143370040+codeasch@users.noreply.github.com>
2024-08-15 19:26:10 +0000

    testing

---
+++ testt.js
@@ -0,0 +1,7 @@
+// This is a simple script to add interactive features
+// Currently, it doesn't have any specific functionality
+// But you can add more features as needed
+
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("Portfolio website loaded!");
+});
